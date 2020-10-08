4d5ee34e27f61b807a6efd7a22a0b7c7147699e6
Dragan Pepic <d.pepic@hotmail.com>
2020-10-08 17:31:53 +0200

    Add project files.

---
+++ GenerikBlaBla/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerikBlaBla
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> listaBrojeva;
+			List<string> listaTxt;
+
+			Zamenjivac<int> zam = new Zamenjivac<int>();
+			zam.Zameni(5, 6);
+
+			Zamenjivac<string> zam2 = new Zamenjivac<string>();
+			zam2.Zameni("asd", "qwe");
+
+			NasaLista<string> lst = new NasaLista<string>();
+			lst.Dodaj("asd");
+			lst.Dodaj("--");
+			lst.Dodaj("grg");
+			lst.Dodaj("s--f-");
+
+			
+
+			for (int brojac = 0; brojac < 20; brojac++)
+				Console.WriteLine(lst.Sledeci());
+			Console.ReadKey();
+
+		}
+	}
+
+	class NasaLista<T>
+	{
+		private T[] niz = new T[0];
+
+		private int trenutniIndeks = 0;
+
+		public void Dodaj(T nesto)
+		{
+			Array.Resize<T>(ref niz, niz.Length + 1);
+			niz[niz.Length - 1] = nesto;
+		}
+
+		public T Sledeci()
+		{
+			if (niz.Length == 0)
+				return default(T);
+			if (trenutniIndeks == niz.Length)
+				trenutniIndeks = 0;
+
+			return niz[trenutniIndeks++];
+		}
+	}
+
+	class Zamenjivac<KoZnaSta>
+	{
+		public void Zameni(KoZnaSta x, KoZnaSta y)
+		{
+			KoZnaSta temp = x;
+			x = y;
+			y = temp;
+		}
+	}
+}
---
+++ GenerikBlaBla/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GenerikBlaBla")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenerikBlaBla")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("47dbd70f-7370-4327-aeea-57157f8e050a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
